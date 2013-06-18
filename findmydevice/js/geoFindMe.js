5c6e09af13148c6a6d46b08bd3ce585c6be2b985
Laurence Barnes <s3069246@student.rmit.edu.au>
2013-06-18 10:18:37 +0200

    add error debugging to script

--- findmydevice/js/geoFindMe.js
+++ findmydevice/js/geoFindMe.js
@@ -1,15 +1,20 @@
-function Location(position)//location function is defining with parameter
-{
-  var latitude = position.coords.latitude; //Specifies the longitude estimate in decimal degrees. The value range is [-180.00, +180.00].
-  var longitude = position.coords.longitude;
-  document.getElementById("lati").innerHTML = latitude; //latitude value is defining in label element where id is lati
-  document.getElementById("longi").innerHTML = longitude;
-  alert("found location");
-}
+//location function is defining with parameter
+function Location(position){
+	var latitude = position.coords.latitude; //Specifies the longitude estimate in decimal degrees. The value range is [-180.00, +180.00].
+	var longitude = position.coords.longitude;
+	document.getElementById("lati").innerHTML = latitude; //latitude value is defining in label element where id is lati
+	document.getElementById("longi").innerHTML = longitude;
+	alert("found location");
+};
+
+function error(error) {
+	output.innerHTML = "Unable to retrieve your location: " + error.message;
+	alert('ERROR(' + error.code + '): ' + error.message);
+};
 
 document.getElementById("submit").addEventListener('click', function findLocation() {
-  if (navigator.geolocation)//checking browser compatibility
-  {
-    navigator.geolocation.getCurrentPosition(Location);//getCurrentPosition method retrieve the current geographic location of the user
-  }
+	//checking browser compatibility
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(Location, error, null);//getCurrentPosition method retrieve the current geographic location of the user
+	}
 });
