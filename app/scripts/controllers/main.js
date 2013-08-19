7b1dc632c417928a3ec6eab1c8e08da43077b9de
Laurence Barnes <s3069246@student.rmit.edu.au>
2013-08-19 11:17:31 +0200

    MediaManager and Geolocation cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,6 @@
 angular.module('findDeviceApp')
 	.controller('MainCtrl', function($scope, $window, geolocation, deviceSettings) {
 
-    /*geolocation.getCurrentPosition(function (position) {
-     alert('Latitude: '              + position.coords.latitude          + '\n' +
-     'Longitude: '             + position.coords.longitude         + '\n' +
-     'Altitude: '              + position.coords.altitude          + '\n' +
-     'Accuracy: '              + position.coords.accuracy          + '\n' +
-     'Altitude Accuracy: '     + position.coords.altitudeAccuracy  + '\n' +
-     'Heading: '               + position.coords.heading           + '\n' +
-     'Speed: '                 + position.coords.speed             + '\n' +
-     'Timestamp: '             + position.timestamp                + '\n');
-     });
-     */
 /*
     var MessageManager = {
 	
--- app/scripts/services/geolocation.js
+++ app/scripts/services/geolocation.js
@@ -1,3 +1,16 @@
+/*geolocation.getCurrentPosition(function (position) {
+ alert(
+    'Latitude: '              + position.coords.latitude          + '\n' +
+    'Longitude: '             + position.coords.longitude         + '\n' +
+    'Altitude: '              + position.coords.altitude          + '\n' +
+    'Accuracy: '              + position.coords.accuracy          + '\n' +
+    'Altitude Accuracy: '     + position.coords.altitudeAccuracy  + '\n' +
+    'Heading: '               + position.coords.heading           + '\n' +
+    'Speed: '                 + position.coords.speed             + '\n' +
+    'Timestamp: '             + position.timestamp                + '\n');
+ });
+ */
+
 'use strict';
 
 angular.module('findDeviceApp').factory('geolocation', function($rootScope) {
